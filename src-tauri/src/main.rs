2d335b9f415b1a4d02f3d49295c9a70fb00c3399
crypdough.eth <106392730+crypdoughdoteth@users.noreply.github.com>
2023-08-28 20:05:25 -0400

    Day 2

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,78 +1,50 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use ethers::{
-    abi::{Address, Token},
-    contract::ContractFactory,
-    core::utils::Anvil,
-    middleware::SignerMiddleware,
-    providers::{Http, Provider},
-    signers::{LocalWallet, Signer, Wallet},
-    types::{Bytes, U256, U128, U64, I256, H128, Uint8},
-};
-use serde::{Serialize, Deserialize};
-use std::{
-    convert::TryFrom, error::Error, fs::File, path::Path, str::FromStr, sync::Arc,
-};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::{fs::File, io::BufReader, path::Path};
 use vyper_rs::vyper::Vyper;
 
 #[derive(Serialize, Deserialize)]
-struct Args {
-    ty: Types, 
-    value: String,
+struct ContractWalletData {
+    keystore: Value,
+    abi: Value,
+    initcode: String,
 }
-
-#[derive(Serialize, Deserialize)]
-enum Types {
-  U256(U256),
-  U128(U128), 
-  U64(U64),
-  U8(Uint8),
-  I256(I256),
-  Address(Address),
-  Bytes, 
-  Bool
+impl ContractWalletData {
+    fn new(keystore: Value, abi: Value, initcode: String) -> ContractWalletData {
+        Self {
+            keystore,
+            abi,
+            initcode,
+        }
+    }
 }
 
 #[tauri::command]
-async fn deploy(
-    provider_url: String,
-    password: String,
-    path: String,
-    key_path: String,
-    args: Vec<Args>
-) -> Result<(), String> {
-      let cpath: &Path = Path::new(path.leak());
-      let new_abi_path = format!("./{:?}_abi.json", cpath.file_name().unwrap());
-      let abi: &Path = Path::new(new_abi_path.leak());
-      let mut contract = Vyper::new(cpath, abi);
-      contract.compile().map_err(|e| return e.to_string())?;
-      contract.abi().map_err(|e| return e.to_string())?;
-      let provider = Provider::<Http>::try_from(provider_url).map_err(|e| e.to_string())?;
-      let wallet =
-          Wallet::decrypt_keystore(&Path::new(&key_path), password).map_err(|e| e.to_string())?;
-      let client = SignerMiddleware::new(provider, wallet);
-      let client = Arc::new(client);
-      let factory = ContractFactory::new(
-          ethers::abi::Contract::load(File::open(contract.abi).map_err(|e| e.to_string())?)
-              .map_err(|e| e.to_string())?,
-          Bytes::from_str(&contract.bytecode.unwrap()).map_err(|e| e.to_string())?,
-          client,
-      );
-      // let tokenized_args = args.iter().map(|a| {
-      //     let t = a.ty; 
-      //     let v = a.value;
-        
-      // }).collect();   
-      //factory.deploy(tokenized_args)?.send().await.map_err(|e| e.to_string())?;
-      Ok(())
-      // password for decryption => keys
-      // contract path
-      // abi
+async fn fetch_data(path: String, key_path: String) -> Result<ContractWalletData, String> {
+    let cpath: &Path = Path::new(path.leak());
+    let new_abi_path = format!("./{:?}_abi.json", cpath.file_name().unwrap());
+    let abi: &Path = Path::new(new_abi_path.leak());
+    let mut contract = Vyper::new(cpath, abi);
+    contract.compile().map_err(|e| return e.to_string())?;
+    contract.abi().map_err(|e| return e.to_string())?;
+    let keyfile = File::open(Path::new(&key_path)).map_err(|e| e.to_string())?;
+    let reader = BufReader::new(keyfile);
+    let keystore_json: Value = serde_json::from_reader(reader).map_err(|e| e.to_string())?;
+    let abifile = File::open(&abi).map_err(|e| e.to_string())?;
+    let reader = BufReader::new(abifile);
+    let abifile_json: Value = serde_json::from_reader(reader).map_err(|e| e.to_string())?;
+    Ok(ContractWalletData::new(
+        keystore_json,
+        abifile_json,
+        contract.bytecode.unwrap(),
+    ))
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![deploy])
+        .invoke_handler(tauri::generate_handler![fetch_data])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/routes/deploy/+page.svelte
+++ src/routes/deploy/+page.svelte
@@ -1,70 +1,115 @@
 <script lang="ts">
-import { invoke } from '@tauri-apps/api/tauri';
+	import { invoke } from '@tauri-apps/api/tauri';
+	import { JsonRpcProvider, ethers } from 'ethers';
 
-    type Args = {
-        ty: Types, 
-        value: String
-    }
-    enum Types {
-        U256,
-        U128, 
-        U64,
-        U8,
-        I256,
-        Address,
-        Bytes, 
-        Bool
-    }
+	type ReturnData = {
+		abi: JSON;
+		keystore: JSON;
+		bytecode: string;
+	};
+	// I
 
-    let contractFile: File;
-    let provider: URL;
-    let keystore: File;
-    let password: String;
-    let args: Array<Args>;
-    function handleSubmit() : void {
-        // callback to Rust code to 
-        // I.   Compile Vyper file
-        // II.  Update provider
-        // III. Decrypt keystore  
-        // IV.  Deploy contract
-        invoke('deploy', {provider_url: provider, password: password, path: contractFile, key_path: keystore})
-    }
+	let contractFile: string;
+	let provider: string;
+	let keystore: string;
+	let password: string;
+	let args: ethers.ContractMethodArgs<any[]>;
+	// O
+	let res: ReturnData;
+
+	async function onSubmit(): Promise<void> {
+		// callback to Rust code to
+		// I.   Compile Vyper file
+		// II.  Update provider
+		// III. Decrypt keystore
+		// IV.  Deploy contract
+    console.log("calling rust code ..... ")
+		invoke<ReturnData>('fetch_data', { path: contractFile, key_path: keystore })
+			.then((message) => (res = message))
+			.catch((error) => console.error(error));
+		let ks = res.keystore;
+		let keys = JSON.stringify(ks);
+		const decryptedWallet = await ethers.Wallet.fromEncryptedJson(keys, password);
+		const abi = new ethers.Interface(JSON.stringify(res.abi));
+		const web3: JsonRpcProvider = new ethers.JsonRpcProvider(provider);
+		const contract = new ethers.ContractFactory(abi, res.bytecode, decryptedWallet.connect(web3));
+		let tx = await contract.deploy(args);
+    console.log(await tx.getAddress());
+    await tx.waitForDeployment();
+	}
 </script>
 
 <div class="navbar rounded-xl">
 	<a href="./" class="btn btn-ghost normal-case text-xl">Home</a>
 </div>
 <div class="flex flex-col justify-center items-center h-screen min-h-screen">
-    <div class="form-control w-full max-w-xs">
-        <!-- svelte-ignore a11y-label-has-associated-control -->
-        <label class="label">
-          <span class="label-text">Vyper Contract</span>
-        </label>
-        <input type="file" bind:value={contractFile} class="file-input file-input-bordered w-full max-w-xs mb-5" />
+	<form on:submit|preventDefault={() => onSubmit()}>
+		<div class="form-control w-full max-w-xs">
+			<!-- svelte-ignore a11y-label-has-associated-control -->
+			<label class="label">
+				<span class="label-text">Vyper Contract</span>
+			</label>
+			<input
+				type="file"
+				bind:value={contractFile}
+				class="file-input file-input-bordered w-full max-w-xs mb-5"
+				required
+			/>
+		</div>
 
-      </div>
-    
-      <div class="form-control w-full max-w-xs">
-        <!-- svelte-ignore a11y-label-has-associated-control -->
-        <label class="label">
-          <span class="label-text">Keystore</span>
-        </label>
-        <input type="file" bind:value={keystore} class="file-input file-input-bordered w-full max-w-xs mb-5" />
-
-      </div>
-    <div class="form-control w-full max-w-xs mb-5">
-		<!-- svelte-ignore a11y-label-has-associated-control -->
-		<label class="label">
-			<span class="label-text">JSON-RPC Provider</span>
-		</label>
-		<input type="text" bind:value={provider} placeholder="|> url of provider" class="input input-bordered w-full max-w-xs" />
-	</div>
-    <div class="form-control w-full max-w-xs mb-5">
-		<!-- svelte-ignore a11y-label-has-associated-control -->
-		<label class="label">
-			<span class="label-text">Decrypt Wallet</span>
-		</label>
-		<input type="text" bind:value={password} placeholder="|> password" class="input input-bordered w-full max-w-xs" />
-	</div>
-    <button class="btn btn-primary justify-center rounded-xl border-8 mt-5" on:click={handleSubmit}>DEPLOY</button>
+		<div class="form-control w-full max-w-xs">
+			<!-- svelte-ignore a11y-label-has-associated-control -->
+			<label class="label">
+				<span class="label-text">Keystore</span>
+			</label>
+			<input
+				type="file"
+				bind:value={keystore}
+				class="file-input file-input-bordered w-full max-w-xs mb-5"
+				required
+			/>
+		</div>
+		<div class="form-control w-full max-w-xs mb-5">
+			<!-- svelte-ignore a11y-label-has-associated-control -->
+			<label class="label">
+				<span class="label-text">Constructor Args</span>
+			</label>
+			<input
+				type="text"
+				bind:value={args}
+				placeholder="|> constructor args"
+				class="input input-bordered w-full max-w-xs"
+				required
+			/>
+		</div>
+		<div class="form-control w-full max-w-xs mb-5">
+			<!-- svelte-ignore a11y-label-has-associated-control -->
+			<label class="label">
+				<span class="label-text">JSON-RPC Provider</span>
+			</label>
+			<input
+				type="text"
+				bind:value={provider}
+				placeholder="|> url of provider"
+				class="input input-bordered w-full max-w-xs"
+				required
+			/>
+		</div>
+		<div class="form-control w-full max-w-xs mb-5">
+			<!-- svelte-ignore a11y-label-has-associated-control -->
+			<label class="label">
+				<span class="label-text">Decrypt Wallet</span>
+			</label>
+			<input
+				type="text"
+				bind:value={password}
+				placeholder="|> password"
+				class="input input-bordered w-full max-w-xs"
+				required
+			/>
+      <button type="submit" class="btn btn-primary justify-center rounded-xl border-8 mt-5"
+			>DEPLOY</button
+		>
+    </div>
+	</form>
 </div>
