f392d74ee22b6b5604dec4a7dd3459cf8c415a6c
lvcky-gg <matadorcrab@gmail.com>
2024-04-09 14:13:46 -0400

    added new page

--- src/pages/create_addresses.tsx
+++ src/pages/create_addresses.tsx
@@ -1,23 +1,34 @@
-import React, { useState }  from "react";
-
-
-
+import React, { useState } from 'react'
 
 export const CreateAddressesPage = () => {
-    const [args, setArgs] = useState<any[]>([]);
-    const [address, setAddress] = useState<string>('');
+  const [args, setArgs] = useState<any[]>([])
+  const [arg, setArg] = useState<any>('')
+
   return (
-    <form>
-        <input
+    <div className='flex flex-col gap-4'>
+      <input
         type='text'
-        onChange={(e) => setAddress(e.target.value)}
-        placeholder="address"
-        >
-        </input>
-      <button onSubmit={() => setArgs([...args, ''])}>Add Constructor Arguments</button>
-      {args.map((i)=>(
-        <h2>{i}</h2>
-      ))}
-    </form>
-  );
-};
+        onChange={e => setArg(e.target.value)}
+        placeholder='Argument'
+        className='input input-bordered'
+      ></input>
+      <button
+        onClick={e => {
+          e.preventDefault()
+          setArgs([...args, arg])
+          setArg('')
+        }}
+      >
+        Add Constructor Arguments
+      </button>
+      <details className='dropdown'>
+        <summary>List All Arguments</summary>
+        <ul>
+          {args.map((i, idx) => (
+            <li key={idx}>{i}</li>
+          ))}
+        </ul>
+      </details>
+    </div>
+  )
+}
