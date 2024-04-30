baf3b2343b229bbb84556f9d2fd192e587bd797c
lvcky-gg <matadorcrab@gmail.com>
2024-04-30 13:46:54 -0400

    made rough draft

--- my-skeleton-app/src/routes/deployContracts/+page.svelte
+++ my-skeleton-app/src/routes/deployContracts/+page.svelte
@@ -9,7 +9,7 @@
 <Stepper on:next={() => handleStep()} class="w-1/3 m-auto mt-24 rounded-sm">
   <Step class="rounded-lg bg-slate-950/20 p-6">
     <svelte:fragment slot="header">Key</svelte:fragment>
-    <form class="w-1/2 m-auto mt-24 h-60">
+    <form class="w-1/2 m-auto mt-20 h-80">
       <label class="label">
         <span>Key To Use</span>
         <select class="select">
@@ -21,12 +21,13 @@
         <span>Input Wallet Password</span>
         <input class="input" type="password" placeholder="password" />
       </label>
+      <button class="btn m-auto block mt-2 bg-slate-950">Show address</button>
     </form>
   </Step>
   <Step class="rounded-lg bg-slate-950/20 p-6">
     <svelte:fragment slot="header">Arguments</svelte:fragment>
 
-    <form class="w-1/2 m-auto mt-24 h-60">
+    <form class="w-1/2 m-auto mt-20 h-80">
       <label class="label mt-4">
         <span>Arguments</span>
         <input class="input mt-8" type="text" placeholder="arguments" />
@@ -34,11 +35,12 @@
       <select class="select mt-10">
         <option value="1">placeholder</option>
       </select>
+      <button class="btn m-auto block mt-2 bg-slate-950">Show args</button>
     </form>
   </Step>
   <Step class=" bg-slate-950/20 p-6 rounded-lg">
     <svelte:fragment slot="header">Contract</svelte:fragment>
-    <form class="w-1/2 m-auto mt-24 h-60">
+    <form class="w-1/2 m-auto mt-20 h-80">
       <label class="label mt-4">
         <span>Contract Type</span>
         <select class="select">
@@ -63,27 +65,7 @@
           <option value="3">London</option>
         </select>
       </label>
+      <button class="btn m-auto block mt-2 bg-slate-950">Estimate Gas</button>
     </form>
   </Step>
 </Stepper>
-
-<!-- <form class="w-1/3 m-auto mt-24">
-
-
-  <p class="mt-4">Place holder for please fill form to see addr</p>
-
-
-  <p class="mt-4">Place holder for list all arguments</p>
-
-
-
-  <p class="mt-4">placeholder for complete form to see gas</p>
-  <div class="flex justify-between">
-    <button type="button" class="btn rounded-lg bg-slate-950 mt-4"
-      >Estimate Gas</button
-    >
-    <button type="button" class="bg-slate-50 btn rounded-lg text-slate-950 mt-4"
-      >Deploy Contract</button
-    >
-  </div>
-</form> -->
