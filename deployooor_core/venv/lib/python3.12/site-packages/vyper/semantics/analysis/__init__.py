fbc9c11c26b941d28707015506d6ea21871a42bc
crypdoughdoteth <zkorn1@icloud.com>
2024-08-27 18:25:20 -0400

    "Clean up"

--- deployooor_core/venv/lib/python3.12/site-packages/vyper/semantics/analysis/__init__.py
+++
@@ -1,5 +0,0 @@
-from .. import types  # break a dependency cycle.
-from .global_ import validate_compilation_target
-from .module import analyze_module
-
-__all__ = [validate_compilation_target, analyze_module]  # type: ignore[misc]
--- deployooor_core/venv/lib/python3.12/site-packages/vyper/venom/passes/sccp/__init__.py
+++
@@ -1,1 +0,0 @@
-from vyper.venom.passes.sccp.sccp import SCCP
